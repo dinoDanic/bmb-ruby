d7014d724dfed030b8192d896686264a320c712c
Dino Danic <dino.danic@gmail.com>
2022-07-13 07:05:33 +0200

    get customers by organization id

--- app/graphql/queries/get_customers_by_organization_id.rb
+++ app/graphql/queries/get_customers_by_organization_id.rb
@@ -1,13 +1,16 @@
 module Queries
     class GetCustomersByOrganizationId < Queries::BaseQuery
       description 'Get organizations customers'
-  
+
+      argument :organization_id, String, required: true
+        
       type [Types::CustomerType], null: false
   
-      def resolve
+      def resolve(organization_id:)
         raise GraphQL::ExecutionError, 'Missing bearer token' if context[:current_user].nil?
-        # customers = Organizati
-        # organizations = User.find(context[:current_user].id).customer
+
+        customers = Organization.find(organization_id).customers
+
       end
     end
   end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,5 +2,6 @@ module Types
   class QueryType < Types::BaseObject
     field :me, resolver: Queries::Me
     field :organizations_get_active, resolver: Queries::OrganizationsGetActive
+    field :get_customers_by_organization_id, resolver: Queries::GetCustomersByOrganizationId
   end
 end
